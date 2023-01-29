80269708a3a6396a45b5a8d9db9380ec5ccf4526
沈金木 <shenjinmu@shenjinmudeMacBook-Pro.local>
2023-01-29 16:57:23 +0800

    fix: optimize secret modify

--- src/kubecube/views/control/config/edit-secret.vue
+++ src/kubecube/views/control/config/edit-secret.vue
@@ -1,128 +1,109 @@
 <template>
-  <validation-observer
-    ref="observer"
-    v-slot="{ invalid }"
-  >
-    <kube-form>
-      <kube-name-input
-        v-model="model.metadata.name"
-        :disabled="isEdit"
-      />
-
-      <kube-form-item
-        label="类型"
-        required
+  <div>
+    <el-form ref="form" :model="model" label-position="right" label-width="120px" style="width:80%">
+      <el-form-item 
+        label="名称" 
+        prop="metadata.name"
+        :rules="[
+          validators.required(),
+          validators.k8sResourceNameValidator()
+        ]"
       >
-        <u-select
+        <el-input v-model="model.metadata.name" :disabled="isEdit" placeholder="1-63位小写字母、数字、或中划线组成，以字母开头，字母或数字结尾"/>
+      </el-form-item>
+      <el-form-item 
+        label="类型" 
+        prop="type"
+        :rules="[
+          validators.required()
+        ]"
+      >
+        <el-select 
           v-model="model.type"
-          size="huge"
-          :data="SECRET_TYPES"
           :disabled="isEdit"
-        />
-      </kube-form-item>
-
-      <template v-if="model.type === 'Opaque'">
-        <kube-form-item
-          label="数据"
-          layout="block"
         >
-          <opaque-input v-model="model.data" />
-        </kube-form-item>
+          <el-option
+            v-for="item in SECRET_TYPES"
+            :key="item.value"
+            :label="item.text"
+            :value="item.value"
+            :title="item.text"
+          />
+        </el-select>
+      </el-form-item>
+      <template v-if="model.type === 'Opaque'">
+        <el-form-item 
+          label="数据" 
+          prop="type"
+        >
+          <opaque-input v-model="model.data" prefixProp="data"/>
+        </el-form-item>
       </template>
       <template v-if="model.type === 'kubernetes.io/tls'">
-        <validation-provider
-          v-slot="{ errors }"
-          name="crt"
-          rules="required"
+        <el-form-item 
+          label="证书内容"
         >
-          <kube-form-item
-            label="证书内容"
-            required
-            layout="block"
-            :message="errors && errors[0]"
+          <el-input
+            v-model="model.dataSource['tls.crt']"
+            type="textarea"
+            :rows="5"
+            placeholder="请输入PEM编码"
+          />
+          <u-uploader
+            :class="$style.uploader"
+            extensions="crt"
+            max-size="1MB"
+            @before-send="onUpload($event, 'tls.crt')"
+            @error="uploadError($event, 'tls.crt')"
           >
-            <u-textarea
-              v-model="model.dataSource['tls.crt']"
-              :class="$style.textarea"
-              :color="errors && errors[0] ? 'error' : ''"
-              resize="none"
-              placeholder="请输入PEM编码"
-            />
-            <u-uploader
-              :class="$style.uploader"
-              extensions="crt"
-              max-size="1MB"
-              @before-send="onUpload($event, 'tls.crt')"
-              @error="uploadError($event, 'tls.crt')"
-            >
-              <u-link>上传文件</u-link>
-            </u-uploader>
-          </kube-form-item>
-        </validation-provider>
-
-        <validation-provider
-          v-slot="{ errors }"
-          name="crtkey"
-          rules="required"
+            <el-link type="primary">上传文件</el-link>
+          </u-uploader>
+          <span ref="tls.crt"></span>
+        </el-form-item>
+        <el-form-item 
+          label="私钥"
         >
-          <kube-form-item
-            label="私钥"
-            required
-            layout="block"
-            :message="errors && errors[0]"
+          <el-input
+            v-model="model.dataSource['tls.key']"
+            type="textarea"
+            :rows="5"
+            placeholder="请输入PEM编码"
+          />
+          <u-uploader
+            :class="$style.uploader"
+            extensions="crt"
+            max-size="1MB"
+            @before-send="onUpload($event, 'tls.key')"
+            @error="uploadError($event, 'tls.key')"
           >
-            <u-textarea
-              v-model="model.dataSource['tls.key']"
-              :class="$style.textarea"
-              :color="errors && errors[0] ? 'error' : ''"
-              resize="none"
-              placeholder="请输入PEM编码"
-            />
-            <u-uploader
-              :class="$style.uploader"
-              extensions="crt"
-              max-size="1MB"
-              @before-send="onUpload($event, 'tls.key')"
-              @error="uploadError($event, 'tls.key')"
-            >
-              <u-link>上传文件</u-link>
-            </u-uploader>
-          </kube-form-item>
-        </validation-provider>
+            <el-link type="primary">上传文件</el-link>
+          </u-uploader>
+          <span ref="tls.key"></span>
+        </el-form-item>
       </template>
       <template v-if="model.type === 'kubernetes.io/dockerconfigjson'">
-        <kube-form-item
-          label="数据"
-          required
-          layout="block"
+        <el-form-item 
+          label="数据" 
+          prop="type"
         >
-          <docker-config v-model="model.dockerData" />
-        </kube-form-item>
+          <docker-config v-model="model.dockerData" prefixProp="dockerData"/>
+        </el-form-item>
       </template>
-      <kube-form-item>
-        <u-submit-button
-          :click="submit.bind(this)"
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="submit"
+          :loading="submitLoading"
         >
-          <template slot-scope="scope">
-            <u-linear-layout>
-              <u-button
-                color="primary"
-                :disabled="invalid || scope.submitting"
-                :icon="scope.submitting ? 'loading' : ''"
-                @click="scope.submit"
-              >
-                {{ isEdit ? "提交设置" : "立即创建" }}
-              </u-button>
-            </u-linear-layout>
-          </template>
-        </u-submit-button>
-      </kube-form-item>
-    </kube-form>
-  </validation-observer>
+          {{ isEdit ? '立即修改' : '立即创建' }}
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
-import { cloneDeep } from 'lodash';
+import { cloneDeep, get as getFunc } from 'lodash';
 import { get } from 'vuex-pathify';
 import workloadService from 'kubecube/services/k8s-resource';
 import {
@@ -135,6 +116,7 @@ import {
 } from 'kubecube/utils/constance';
 import opaqueInput from './opaque-input.vue';
 import dockerConfig from './docker-config.vue';
+import * as validators from 'kubecube/utils/validators';
 
 export default {
     components: {
@@ -148,6 +130,8 @@ export default {
         return {
             SECRET_TYPES,
             model: cloneDeep(this.instance) || toSecretPlainObject(),
+            validators,
+            submitLoading: false,
         };
     },
     computed: {
@@ -159,6 +143,8 @@ export default {
     },
     methods: {
         onUpload(event, key) {
+            const ref = this.$refs[key];
+            ref.innerText = '';
             event.preventDefault();
             const reader = new FileReader();
             reader.readAsText(event.file);
@@ -169,35 +155,52 @@ export default {
         uploadError(e, key) {
             const ref = this.$refs[key];
             ref.color = 'error';
-            if (e.name === 'ExtensionError') { ref.currentMessage = `只能上传 ${e.extensions.join(', ')} 类型的文件！`; } else { ref.currentMessage = e.message; }
+            if (e.name === 'ExtensionError') { ref.innerText = `只能上传 ${e.extensions.join(', ')} 类型的文件！`; } else { ref.innerText = e.message; }
             this.model.dataSource[key] = '';
         },
         async submit() {
-            if (this.isEdit) {
-                const yaml = toPatchSecretObject(this.model);
-                console.log(yaml);
-                await workloadService.patchAPIV1Instance({
-                    pathParams: {
-                        cluster: this.cluster,
-                        namespace: this.namespace,
-                        resource: 'secrets',
-                        name: this.instance.metadata.name,
-                    },
-                    data: yaml,
-                });
-            } else {
-                const yaml = toSecretK8SObject(this.model);
-                await workloadService.createAPIV1Instance({
-                    pathParams: {
-                        cluster: this.cluster,
-                        namespace: this.namespace,
-                        resource: 'secrets',
-                    },
-                    data: yaml,
-                });
+            try {
+                await this.$refs.form.validate();
+            } catch (error) {
+                console.log(error);
+                return;
             }
-
-            this.$router.push({ path: '/control/secrets/list' });
+            this.submitLoading = true;
+            try {
+                if (this.isEdit) {
+                    const puresource = this.model.puresource;
+                    const yaml = toPatchSecretObject(this.model);
+                    puresource.data = yaml.data;
+                    await workloadService.modifyAPIV1Instance({
+                        pathParams: {
+                            cluster: this.cluster,
+                            namespace: this.namespace,
+                            resource: 'secrets',
+                            name: this.instance.metadata.name,
+                        },
+                        data: puresource,
+                        noAlert: true,
+                    });
+                } else {
+                    const yaml = toSecretK8SObject(this.model);
+                    await workloadService.createAPIV1Instance({
+                        pathParams: {
+                            cluster: this.cluster,
+                            namespace: this.namespace,
+                            resource: 'secrets',
+                        },
+                        data: yaml,
+                        noAlert: true,
+                    });
+                }
+                this.$router.push({ path: '/control/secrets/list' });
+            } catch (err) {
+                this.$globalErrorModal(getFunc(err, 'details.causes[0]') || err);
+            }
+            this.submitLoading = false;
+        },
+        handleValidate() {
+          this.$refs.observer.validate();
         },
     },
 };
@@ -230,5 +233,11 @@ export default {
 }
 .uploader[class] {
     display: block;
+}
+.question::after {
+    font-size: 16px;
+    color: #FF4D4F;
+    icon-font: url('kubecube/assets/question.svg');
+    cursor: pointer;
 }
 </style>
--- src/kubecube/views/control/config/opaque-input.vue
+++ src/kubecube/views/control/config/opaque-input.vue
@@ -1,61 +1,57 @@
 <template>
-  <kube-dynamic-block
-    v-model="model"
-    style="width: 578px"
-    :data-template="getDataTemplate"
-  >
-    <template slot="column">
-      <th width="33.3333%">
-        Key
-      </th>
-      <th width="66.6666%">
-        Value
-      </th>
-    </template>
-    <template slot-scope="{ model, index }">
-      <td>
-        <validation-provider
-          v-slot="{ errors }"
-          :name="`Key-${index}`"
-          rules="ConsistofNormalSymbol"
+  <div>
+    <dynamicBlock
+      v-model="model"
+      :getDefaultItem="getDataTemplate"
+      :columns="[
+          {
+              title: 'Key',
+              dataIndex: 'key',
+          },
+          {
+              title: 'Value',
+              dataIndex: 'value',
+          }
+      ]"
+    >
+      <template v-slot:key="{record: dataModel, index: dataIndex}">
+        <el-form-item 
+          label=""
+          :prop="`${prefixProp}.${dataIndex}.key`"
+          :rules="[
+            validators.consistofNormalSymbol(false),
+          ]"
         >
-          <kube-form-item
-            muted="no"
-            style="width: 100%;"
-            field-size="full"
-            layout="none"
-            :message="errors && errors[0]"
-            placement="bottom"
-          >
-            <u-input
-              v-model="model.key"
-              size="normal huge"
-              placeholder="Key 由数字、字母、'-'、 '_' 或'.'组成"
-              :disabled="model.disabled"
-              :color="errors && errors[0] ? 'error' : ''"
-            />
-          </kube-form-item>
-        </validation-provider>
-      </td>
-      <td>
-        <div
-          :class="$style.wrap"
-        >
-          <u-textarea
-            v-model="model.value"
-            :class="$style.textarea"
-            resize="none"
-          />
-        </div>
-      </td>
-    </template>
-  </kube-dynamic-block>
+          <el-input v-model="dataModel.key" placeholder="Key 由数字、字母、'-'、 '_' 或'.'组成"/>
+        </el-form-item>
+      </template>
+      <template v-slot:value="{record: dataModel}">
+        <el-input v-model="dataModel.value"/>
+      </template>
+    </dynamicBlock>
+  </div>
 </template>
 
 <script>
 import { makeVModelMixin } from 'kubecube/mixins/functional';
+import dynamicBlock from 'kubecube/elComponent/dynamic-block/index.vue';
+import * as validators from 'kubecube/utils/validators';
 export default {
+    components: {
+      dynamicBlock
+    },
+    props: {
+      prefixProp: {
+        type: String,
+        default: ''
+      }
+    },
     mixins: [ makeVModelMixin ],
+    data() {
+      return {
+        validators,
+      }
+    },
     computed: {
         exsitKeys() {
             return this.model.map(t => t.key);
